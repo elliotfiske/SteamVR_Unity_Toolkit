70ca01356d1659aa9accb5aed804ef18943d5f8b
Elliot Fiske <elliotfiske@gmail.com>
2016-09-15 13:22:34 -0700

    lol that looks neato

---
+++ Assets/RemoveMeAfterXSeconds.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoveMeAfterXSeconds : MonoBehaviour {
+
+    public double seconds;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        seconds -= Time.deltaTime;
+        if (seconds < 0) {
+            GameObject.Destroy(this.gameObject);
+        }
+	}
+}
